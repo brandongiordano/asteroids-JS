876d2b10bc2d8e40a2cc1ada21159af690401a82
brandongiordano <bgiordano9917@aol.com>
2023-10-06 19:45:41 -0400

    asteroids and missiles rendering

--- asteroids.js
+++ asteroids.js
@@ -4,7 +4,7 @@ let canvasWidth = 1400;
 let canvasHeight = 1000;
 let ship;
 let keys = [];
-let missles = [];
+let missiles = [];
 let asteroids = [];
 
 document.addEventListener('DOMContentLoaded', setupCanvas);
@@ -18,11 +18,19 @@ function setupCanvas() {
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ship = new Ship();
+
+    for(let i = 0; i < 8; i++) {
+        asteroids.push(new Asteroid());
+    }
+
     document.body.addEventListener("keydown", function(e) {
         keys[e.keyCode] = true;
     });
     document.body.addEventListener("keyup", function(e) {
         keys[e.keyCode] = false;
+        if(e.keyCode === 32) {
+           missiles.push(new Missile(ship.angle * (180 / Math.PI))); 
+        }
     });
     render();
 }
@@ -42,6 +50,9 @@ class Ship {
         this.radius = 15;
         this.angle = 0;
         this.strokeColor = 'white';
+        // nose cannon position
+        this.noseX = canvasWidth / 2 + 15;
+        this.noseY = canvasHeight / 2;
     }
     // functions simulate movement
     rotate(dir) {
@@ -53,6 +64,7 @@ class Ship {
             this.velX += Math.cos(radians) * this.speed;
             this.velY += Math.sin(radians) * this.speed;
         }
+        // makes ship appear on opposite side
         if (this.x < -this.radius) {
             this.x = canvasWidth + this.radius;
         }
@@ -78,6 +90,8 @@ class Ship {
         // making triangle
         let vertAngle = ((Math.PI * 2) / 3);
         let radians = this.angle;
+        this.noseX = this.x - this.radius * Math.cos(radians);
+        this.noseY = this.y - this.radius * Math.sin(radians);
         for (let i = 0; i < 3; i++) {
             ctx.lineTo(
                 this.x - this.radius * Math.cos(vertAngle * i + radians),
@@ -89,7 +103,74 @@ class Ship {
     }
 }
 
-// let ship = new Ship();
+// missile blueprint
+class Missile {
+    constructor(angle) {
+        this.visible = true;
+        this.x = ship.noseX;
+        this.y = ship.noseY;
+        this.angle = angle;
+        this.width = 4;
+        this.height = 4;
+        this.speed = 5;
+        this.velX = 0;
+        this.velY = 0;
+    }
+    update() {
+        var radians = this.angle * Math.PI / 180;
+        this.x -= Math.cos(radians) * this.speed;
+        this.y -= Math.sin(radians) * this.speed;
+    }
+    draw() {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+class Asteroid {
+    constructor(x, y) {
+        this.visible = true;
+        this.x = Math.floor(Math.random() * canvasWidth);
+        this.y = Math.floor(Math.random() * canvasHeight);
+        this.speed = 1;
+        this.radius = 50;
+        this.angle = Math.floor(Math.random() * 359);
+        this.strokeColor = 'white';
+    }
+    update() {
+        var radians = this.angle * Math.PI / 180;
+        this.x += Math.cos(radians) * this.speed;
+        this.y += Math.sin(radians) * this.speed;
+        // makes asteroid appear on opposite side
+        if (this.x < -this.radius) {
+            this.x = canvasWidth + this.radius;
+        }
+        if (this.x > canvasWidth + this.radius) {
+            this.x = -this.radius;
+        }
+        if (this.y < -this.radius) {
+            this.y = canvasHeight + this.radius;
+        }
+        if (this.y > canvasHeight + this.radius) {
+            this.y = -this.radius;
+        }
+    }
+    // draws the asteroid on screen
+    draw() {
+        ctx.beginPath();
+        // making hexagon
+        let vertAngle = ((Math.PI * 2) / 6);
+        let radians = this.angle / Math.PI * 180;
+        for (let i = 0; i < 6; i++) {
+            ctx.lineTo(
+                this.x - this.radius * Math.cos(vertAngle * i + radians),
+                this.y - this.radius * Math.sin(vertAngle * i + radians)
+            );
+        }
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
 
 function render() {
     ship.movingForward = keys[87];
@@ -102,5 +183,17 @@ function render() {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     ship.update();
     ship.draw();
+    if(missiles.length !== 0) {
+        for (let i = 0; i < missiles.length; i++) {
+            missiles[i].update();
+            missiles[i].draw();
+        }
+    }
+    if(asteroids.length !== 0) {
+        for (let j = 0; j < asteroids.length; j++) {
+            asteroids[j].update();
+            asteroids[j].draw();
+        }
+    }
     requestAnimationFrame(render);
 }
